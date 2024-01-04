ae025e03c83a504caf2d39267d5f74705c2eb4f6
Lars Kjær Rasmussen <laras@morningtrain.dk>
2024-01-04 14:27:08 +0100

    ✨ Add Product Service

---
+++ src/woocommerce/ProductService.php
@@ -0,0 +1,25 @@
+<?php
+namespace MorningTrain\WoocommerceEconomic\Woocommerce;
+
+
+class ProductService
+{
+    public static function addEconomicProductField(): void
+    {
+        woocommerce_wp_text_input([
+            'id' => 'economic_product_id',
+            'label' => __('E-conomic produkt id', 'mt-wc-economic'),
+            'placeholder' => __('E-conomic produkt id', 'mt-wc-economic'),
+            'desc_tip' => 'true',
+            'description' => __('Udfyld varenummerer fra e-conomic', 'mt-wc-economic'),
+            'type' => 'number',
+        ]);
+    }
+
+    public static function saveEconomicProductField($post_id): void
+    {
+        $economic_product_id = $_POST['economic_product_id'] ?? '';
+        update_post_meta($post_id, 'economic_product_id', sanitize_text_field($economic_product_id));
+    }
+
+}
